9dceb7b7233182bd171e469f16d06c1a9327bda7
ritik1412 <ritik.10.bhargav@gmail.com>
2022-07-14 15:21:44 +0530

    doc

--- lib/MediNeed/Doctors/general.dart
+++ lib/MediNeed/Doctors/general.dart
@@ -37,176 +37,179 @@ class _feverState extends State<fever> {
           ),
           backgroundColor: Color.fromARGB(255, 42, 114, 178),
         ),
-        body: Container(
-          padding: EdgeInsets.all(18), //insert space within
-          margin: EdgeInsets.only(bottom: 10),
-          child: Center(
-            //insert space within
-
-            child: ListView(
-              children: [
-                Text("Best Doctor In Suggestion.",
-                    style: TextStyle(
-                      fontSize: 26,
-                      color: Colors.black87,
-                      fontFamily: "Roboto",
-                      fontWeight: FontWeight.bold,
-                    )),
-                SizedBox(
-                  height: 10,
-                ),
-                Container(
-                    padding: EdgeInsets.all(38), //insert space within
+        body: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ListView(
+            children: [
+              Text("Best Doctor In Suggestion.",
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.black87,
+                    fontFamily: "Roboto",
+                    fontWeight: FontWeight.bold,
+                  )),
+              SizedBox(
+                height: 10,
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: Container(
+                    padding: EdgeInsets.all(8), //insert space within
                     margin: EdgeInsets.only(bottom: 10),
                     decoration: BoxDecoration(
                         border: Border.all(),
                         borderRadius: BorderRadius.circular(20)),
                     child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        Image(
-                          image: AssetImage("images/d1.jpg"),
-                          height: 180,
-                          width: 130,
-                          fit: BoxFit.cover,
+                        ClipRRect(
+                          borderRadius: BorderRadius.circular(12.0),
+                          child: Image.asset(
+                            "images/d1.jpg",
+                            height: 120.0,
+                            width: 100.0,
+                          ),
                         ),
                         Container(
                           padding:
-                              EdgeInsets.only(left: 58), //insert space within
+                              EdgeInsets.only(left: 10), //insert space within
 
-                          child: Column(children: [
-                            Text(
-                              "Dr.Anand Sachdeva",
-                              style: TextStyle(
-                                fontSize: 20,
-                                fontFamily: "Oswald",
-                              ),
-                            ),
-                            Text("General Physician.",
-                                style: TextStyle(
+                          child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Dr.Anand Sachdeva",
+                                  style: TextStyle(
                                     fontSize: 18,
-                                    fontFamily: "Ibarra Real Nova",
-                                    color: Color.fromARGB(95, 26, 25, 25),
-                                    decoration: TextDecoration.underline)),
-                            Text("10 years experience ",
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontFamily: "Ibarra Real Nova",
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w600,
-                                )),
-                            RichText(
-                                text: TextSpan(children: [
-                              WidgetSpan(
-                                  child: Icon(
-                                Icons.star,
-                                color: Color.fromARGB(255, 40, 178, 45),
-                                size: 30,
-                              )),
-                              TextSpan(
-                                  text: "4.9",
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.w900,
-                                      fontSize: 20)),
-                              TextSpan(
-                                  text: "  Rated by 2000+ patients",
-                                  style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w400)),
-                            ])),
-                            Text("Qualification:",
-                                style: TextStyle(
-                                    fontSize: 20, fontWeight: FontWeight.w700)),
-                            Text("MBBS",
-                                style: TextStyle(
-                                    fontSize: 20, fontWeight: FontWeight.w400)),
-                          ]),
+                                    fontFamily: "Oswald",
+                                  ),
+                                ),
+                                Text("General Physician.",
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontFamily: "Ibarra Real Nova",
+                                        color: Color.fromARGB(95, 26, 25, 25),
+                                        decoration: TextDecoration.underline)),
+                                Text("10 years experience ",
+                                    style: TextStyle(
+                                      fontSize: 14,
+                                      fontFamily: "Ibarra Real Nova",
+                                      color: Colors.black,
+                                      fontWeight: FontWeight.w600,
+                                    )),
+                                RichText(
+                                    text: TextSpan(children: [
+                                  WidgetSpan(
+                                      child: Icon(
+                                    Icons.star,
+                                    color: Color.fromARGB(255, 40, 178, 45),
+                                    size: 30,
+                                  )),
+                                  TextSpan(
+                                      text: "  4.9",
+                                      style: TextStyle(fontSize: 16)),
+                                  TextSpan(
+                                      text: " Rated by\n2000+ patients ",
+                                      style: TextStyle(
+                                          fontSize: 16,
+                                          fontWeight: FontWeight.w400)),
+                                ])),
+                                Text("Qualification:",
+                                    style: TextStyle(
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w600)),
+                                Text("MBBS",
+                                    style: TextStyle(
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.w200)),
+                              ]),
                         ),
                       ],
                     )),
-                Container(
-                  padding: EdgeInsets.only(left: 20), //insert space within
-                  alignment: Alignment.bottomLeft,
-                  child: Column(children: [
-                    RichText(
-                        text: TextSpan(children: [
-                      WidgetSpan(
-                          child: Icon(
-                        Icons.textsms,
-                        color: Color.fromARGB(255, 40, 91, 178),
-                        size: 30,
-                      )),
-                      TextSpan(
-                          text: " Speaks:English",
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w400))
-                    ])),
-                    RichText(
-                        text: TextSpan(children: [
-                      WidgetSpan(
-                          child: Icon(
-                        Icons.duo,
-                        color: Color.fromARGB(255, 40, 91, 178),
-                        size: 30,
-                      )),
-                      TextSpan(
-                          text: " 6442 consultations",
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w400))
-                    ])),
-                    RichText(
-                        text: TextSpan(children: [
-                      WidgetSpan(
-                          child: Icon(
-                        Icons.account_balance_wallet,
-                        color: Color.fromARGB(255, 40, 91, 178),
-                        size: 30,
-                      )),
-                      WidgetSpan(
-                          child: Icon(
-                        Icons.currency_rupee,
-                        color: Colors.black,
-                        size: 20,
-                      )),
-                      TextSpan(
-                          text: "449 Consultation fee",
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.w400))
-                    ])),
-                  ]),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                Center(
-                  child: Text("Select any slot to book consultation",
-                      style:
-                          TextStyle(fontSize: 20, fontWeight: FontWeight.w800)),
-                ),
-                Container(
-                    margin: EdgeInsets.only(top: 10),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        ElevatedButton(
-                            onPressed: () {},
-                            child: Text("4:30 PM",
-                                style: TextStyle(fontWeight: FontWeight.w400))),
-                        ElevatedButton(
-                            onPressed: () {},
-                            child: Text("6:00 PM",
-                                style: TextStyle(fontWeight: FontWeight.w400))),
-                        ElevatedButton(
-                            onPressed: () {},
-                            child: Text("7:30 PM",
-                                style: TextStyle(fontWeight: FontWeight.w400))),
-                        ElevatedButton(
-                            onPressed: () {},
-                            child: Text("9:00 PM",
-                                style: TextStyle(fontWeight: FontWeight.w400)))
-                      ],
+              ),
+              Container(
+                padding: EdgeInsets.only(left: 20), //insert space within
+                alignment: Alignment.bottomLeft,
+                child: Column(children: [
+                  RichText(
+                      text: TextSpan(children: [
+                    WidgetSpan(
+                        child: Icon(
+                      Icons.textsms,
+                      color: Color.fromARGB(255, 40, 91, 178),
+                      size: 30,
                     )),
-              ],
-            ),
+                    TextSpan(
+                        text: " Speaks:English",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w400))
+                  ])),
+                  RichText(
+                      text: TextSpan(children: [
+                    WidgetSpan(
+                        child: Icon(
+                      Icons.duo,
+                      color: Color.fromARGB(255, 40, 91, 178),
+                      size: 30,
+                    )),
+                    TextSpan(
+                        text: " 6442 consultations",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w400))
+                  ])),
+                  RichText(
+                      text: TextSpan(children: [
+                    WidgetSpan(
+                        child: Icon(
+                      Icons.account_balance_wallet,
+                      color: Color.fromARGB(255, 40, 91, 178),
+                      size: 30,
+                    )),
+                    WidgetSpan(
+                        child: Icon(
+                      Icons.currency_rupee,
+                      color: Colors.black,
+                      size: 20,
+                    )),
+                    TextSpan(
+                        text: "449 Consultation fee",
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w400))
+                  ])),
+                ]),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Center(
+                child: Text("Select any slot to book consultation",
+                    style:
+                        TextStyle(fontSize: 20, fontWeight: FontWeight.w800)),
+              ),
+              Container(
+                  margin: EdgeInsets.only(top: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      ElevatedButton(
+                          onPressed: () {},
+                          child: Text("4:30 PM",
+                              style: TextStyle(fontWeight: FontWeight.w400))),
+                      ElevatedButton(
+                          onPressed: () {},
+                          child: Text("6:00 PM",
+                              style: TextStyle(fontWeight: FontWeight.w400))),
+                      ElevatedButton(
+                          onPressed: () {},
+                          child: Text("7:30 PM",
+                              style: TextStyle(fontWeight: FontWeight.w400))),
+                      ElevatedButton(
+                          onPressed: () {},
+                          child: Text("9:00 PM",
+                              style: TextStyle(fontWeight: FontWeight.w400)))
+                    ],
+                  )),
+            ],
           ),
         ));
   }
